2b16852913530158594570dcb9e982419ffb0ab0
dsv <zxcv>
2024-10-01 18:57:28 +0500

    Initial Commit

---
+++ InventoryTrack/InventoryTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryTrackApp.swift
+//  InventoryTrack
+//
+//  Created by Muhammad Rizwan on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct InventoryTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
